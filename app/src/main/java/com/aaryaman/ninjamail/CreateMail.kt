183101c9ead8ecbd3fd8bc5ef9d3752ff15eafd2
Aaryaman-Bhardwaj <61933492+Aaryaman-Bhardwaj@users.noreply.github.com>
2021-06-26 10:31:19 +0530

    initial commit

---
+++ app/src/main/java/com/aaryaman/ninjamail/CreateMail.kt
@@ -0,0 +1,35 @@
+package com.aaryaman.ninjamail
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+class CreateMail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_mail)
+
+
+
+
+    }
+
+    fun sendEmail() {
+        //Alternate way of instantiating
+        //SendGrid sendGrid = new SendGrid(SENDGRID_USERNAME,SENDGRID_PASSWORD);
+
+        val api: String? = ""
+        //Instantiate the object using your API key String
+        val sendgrid = SendGrid(api)
+        val email = SendGrid.Email()
+        email.addTo("1905827@kiit.ac.in")
+        email.from = "other@example.com"
+        email.subject = "Hello World"
+        email.text = "My first email with SendGrid Java!"
+        try {
+            val response = sendgrid.send(email)
+        } catch (e: SendGridException) {
+            Log.e("sendError", "Error sending email")
+        }
+    }
+}
